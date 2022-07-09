3599dc9c8e35db3b3951c7d48c55d3c6d6649d3b
donghwan <ldh793@naver.com>
2022-07-09 19:54:15 +0900

    Todo list part1

---
+++ react/base/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => {setToDo(event.target.value)}
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(toDo)
+    if (toDo === ''){
+      return 
+    }
+    setToDos(currentArray => [...currentArray, toDo])
+    setToDo('')  
+  }
+  console.log(toDos)
+
+  return (
+    <div>
+      <h1>My Todos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} type='text' onChange={onChange} ></input>
+        <button>Add TODO</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
